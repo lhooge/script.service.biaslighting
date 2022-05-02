2e68b11a69870a487f47fb334830ce65d8fe0a61
Lars Hoogestraat <lars@hoogi.eu>
2022-05-02 22:00:44 +0200

    compatibility for kodi 19

--- addon.py
+++ addon.py
@@ -4,19 +4,15 @@ import xbmc
 import xbmcaddon
 from PIL import Image
 from resources.lib.mote import Mote
+from settings import Settings, Direction
 
 NUM_PIXEL = 16
 
-colors = [
-    (0, 0, 0),      # None
-    (255, 0, 0),    # Red
-    (0, 255, 0),    # Green
-    (0, 0, 255),    # Blue
-    (255, 255, 0),  # Yellow
-    (255, 255, 255) # White
-]
+WIDTH, HEIGHT = 32, 32
 
 mote = Mote()
+
+mote.clear()
 
 mote.configure_channel(1, NUM_PIXEL, True)
 mote.configure_channel(2, NUM_PIXEL, True)
@@ -26,269 +22,89 @@ mote.configure_channel(4, NUM_PIXEL, True)
 mote.set_clear_on_exit()
 
 
-class Position():
-    top = 1
-    bottom = 2
-    left = 3
-    right = 4
-
-
-def set_static_color(channels, index, r, g, b, brightness=None):
-    for k, v in channels.items():
-        mote.set_pixel(v.get("channel"), index, r, g, b, brightness)
-
-
 def clear_mote():
     mote.clear()
     mote.show()
 
 
-monitor = xbmc.Monitor()
+def set_static_color(channels, index, color, brightness=None):
+    for k, v in channels.items():
+        mote.set_pixel(k, index, color.red, color.green, color.blue, brightness)
 
-width, height = 32, 32
-settings = xbmcaddon.Addon(id='script.service.biaslightmote')
 
+class BiasLightingMonitor(xbmc.Monitor):
 
-mote.clear()
+    def __init__(self):
+        super(BiasLightingMonitor, self).__init__()
+        self.settings = Settings()
 
-mote1 = settings.getSetting("mote1")
-mote1_pos = int(settings.getSetting("mote1_position"))
-mote1_inv = settings.getSetting("mote1_invert")
+    def onSettingsChanged(self):
+        xbmc.log(msg="ao", level=xbmc.LOGINFO)
+        self.settings.readSettings()
+        clear_mote()
 
-mote2 = settings.getSetting("mote2")
-mote2_pos = int(settings.getSetting("mote2_position"))
-mote2_inv = settings.getSetting("mote2_invert")
+    def onAbortRequested(self):
+        clear_mote()
 
-mote3 = settings.getSetting("mote3")
-mote3_pos = int(settings.getSetting("mote3_position"))
-mote3_inv = settings.getSetting("mote3_invert")
+    def main(self):
+        channels = self.settings.get_channels()
 
-mote4 = settings.getSetting("mote4")
-mote4_pos = int(settings.getSetting("mote4_position"))
-mote4_inv = settings.getSetting("mote4_invert")
+        if self.settings.get_mode() == 0:
+            if self.settings.get_color() == 0:
+                clear_mote()
+            for c in channels.items():
+                for pixel in range(NUM_PIXEL):
+                    set_static_color(channels, pixel, self.settings.get_color(), self.settings.get_brightness())
+            mote.show()
 
-mode = int(settings.getSetting("mode"))
-color = int(settings.getSetting("color"))
+        elif self.settings.get_mode() == 1:
+            while not self.abortRequested():
+                if self.waitForAbort(0.6):
+                    break
 
-custom_color_red = int(settings.getSetting("color_custom_red"))
-custom_color_green = int(settings.getSetting("color_custom_green"))
-custom_color_blue = int(settings.getSetting("color_custom_blue"))
-colors.append((custom_color_red, custom_color_green, custom_color_blue))
+                capture = xbmc.RenderCapture()
+                capture.capture(WIDTH, HEIGHT)
+                pixels = capture.getImage(1000)
 
-brightness = float(float(settings.getSetting("brightness")) / float(100))
+                if not pixels:
+                    if self.settings.get_color() == 0:
+                        clear_mote()
+                    else:
+                        for pixel in range(NUM_PIXEL):
+                            set_static_color(channels, pixel, self.settings.get_color(), self.settings.get_brightness())
+                        mote.show()
+                else:
+                    image = Image.frombytes("RGBA", (WIDTH, HEIGHT), bytes(pixels), "raw", "BGRA")
 
-channels = {}
+                    for k, v in channels.items():
+                        channel = k
 
-if mote1 == "true":
-    channels.update({"1": {"channel": 1, "invert": mote1_inv}})
-    # TOP LEFT
-    if mote1_pos == 0:
-        channels.get("1").update({"direction": Position.top, "start": 0, "stop": 16})
-    # TOP MIDDLE
-    elif mote1_pos == 1:
-        channels.get("1").update({"direction": Position.top, "start": 8, "stop": 24})
-    # TOP RIGHT
-    elif mote1_pos == 2:
-        channels.get("1").update({"direction": Position.top, "start": 16, "stop": 32})
-    # SIDE LEFT TOP
-    elif mote1_pos == 3:
-        channels.get("1").update({"direction": Position.left, "start": 0, "stop": 16})
-    # SIDE LEFT MIDDLE
-    elif mote1_pos == 4:
-        channels.get("1").update({"direction": Position.left, "start": 8, "stop": 24})
-    # SIDE LEFT BOTTOM
-    elif mote1_pos == 5:
-        channels.get("1").update({"direction": Position.left, "start": 16, "stop": 32})
-    # SIDE RIGHT TOP
-    elif mote1_pos == 6:
-        channels.get("1").update({"direction": Position.right, "start": 0, "stop": 16})
-    # SIDE RIGHT MIDDLE
-    elif mote1_pos == 7:
-        channels.get("1").update({"direction": Position.right, "start": 8, "stop": 24})
-    # SIDE RIGHT BOTTOM
-    elif mote1_pos == 8:
-        channels.get("1").update({"direction": Position.right, "start": 16, "stop": 32})
-    # BOTTOM LEFT
-    elif mote1_pos == 9:
-        channels.get("1").update({"direction": Position.bottom, "start": 0, "stop": 16})
-    # BOTTOM MIDDLE
-    elif mote1_pos == 10:
-        channels.get("1").update({"direction": Position.bottom, "start": 8, "stop": 24})
-    # BOTTOM RIGHT
-    elif mote1_pos == 11:
-        channels.get("1").update({"direction": Position.bottom, "start": 16, "stop": 32})
-if mote2 == "true":
-    channels.update({"2": {"channel": 2, "invert": mote2_inv}})
-    # TOP LEFT
-    if mote2_pos == 0:
-        channels.get("2").update({"direction": Position.top, "start": 0, "stop": 16})
-    # TOP MIDDLE
-    elif mote2_pos == 1:
-        channels.get("2").update({"direction": Position.top, "start": 8, "stop": 24})
-    # TOP RIGHT
-    elif mote2_pos == 2:
-        channels.get("2").update({"direction": Position.top, "start": 16, "stop": 32})
-    # SIDE LEFT TOP
-    elif mote2_pos == 3:
-        channels.get("2").update({"direction": Position.left, "start": 0, "stop": 16})
-    # SIDE LEFT MIDDLE
-    elif mote2_pos == 4:
-        channels.get("2").update({"direction": Position.left, "start": 8, "stop": 24})
-    # SIDE LEFT BOTTOM
-    elif mote2_pos == 5:
-        channels.get("2").update({"direction": Position.left, "start": 16, "stop": 32})
-    # SIDE RIGHT TOP
-    elif mote2_pos == 6:
-        channels.get("2").update({"direction": Position.right, "start": 0, "stop": 16})
-    # SIDE RIGHT MIDDLE
-    elif mote2_pos == 7:
-        channels.get("2").update({"direction": Position.right, "start": 8, "stop": 24})
-    # SIDE RIGHT BOTTOM
-    elif mote2_pos == 8:
-        channels.get("2").update({"direction": Position.right, "start": 16, "stop": 32})
-    # BOTTOM LEFT
-    elif mote2_pos == 9:
-        channels.get("2").update({"direction": Position.bottom, "start": 0, "stop": 16})
-    # BOTTOM MIDDLE
-    elif mote2_pos == 10:
-        channels.get("2").update({"direction": Position.bottom, "start": 8, "stop": 24})
-    # BOTTOM RIGHT
-    elif mote2_pos == 11:
-        channels.get("2").update({"direction": Position.bottom, "start": 16, "stop": 32})
-if mote3 == "true":
-    channels.update({"3": {"channel": 3, "invert": mote3_inv}})
-    # TOP LEFT
-    if mote3_pos == 0:
-        channels.get("3").update({"direction": Position.top, "start": 0, "stop": 16})
-    # TOP MIDDLE
-    elif mote3_pos == 1:
-        channels.get("3").update({"direction": Position.top, "start": 8, "stop": 24})
-    # TOP RIGHT
-    elif mote3_pos == 2:
-        channels.get("3").update({"direction": Position.top, "start": 16, "stop": 32})
-    # SIDE LEFT TOP
-    elif mote3_pos == 3:
-        channels.get("3").update({"direction": Position.left, "start": 0, "stop": 16})
-    # SIDE LEFT MIDDLE
-    elif mote3_pos == 4:
-        channels.get("3").update({"direction": Position.left, "start": 8, "stop": 24})
-    # SIDE LEFT BOTTOM
-    elif mote3_pos == 5:
-        channels.get("3").update({"direction": Position.left, "start": 16, "stop": 32})
-    # SIDE RIGHT TOP
-    elif mote3_pos == 6:
-        channels.get("3").update({"direction": Position.right, "start": 0, "stop": 16})
-    # SIDE RIGHT MIDDLE
-    elif mote3_pos == 7:
-        channels.get("3").update({"direction": Position.right, "start": 8, "stop": 24})
-    # SIDE RIGHT BOTTOM
-    elif mote3_pos == 8:
-        channels.get("3").update({"direction": Position.right, "start": 16, "stop": 32})
-    # BOTTOM LEFT
-    elif mote3_pos == 9:
-        channels.get("3").update({"direction": Position.bottom, "start": 0, "stop": 16})
-    # BOTTOM MIDDLE
-    elif mote3_pos == 10:
-        channels.get("3").update({"direction": Position.bottom, "start": 8, "stop": 24})
-    # BOTTOM RIGHT
-    elif mote3_pos == 11:
-        channels.get("3").update({"direction": Position.bottom, "start": 16, "stop": 32})
-if mote4 == "true":
-    channels.update({"4": {"channel": 4, "invert": mote4_inv}})
-    # TOP LEFT
-    if mote4_pos == 0:
-        channels.get("4").update({"direction": Position.top, "start": 0, "stop": 16})
-    # TOP MIDDLE
-    elif mote4_pos == 1:
-        channels.get("4").update({"direction": Position.top, "start": 8, "stop": 24})
-    # TOP RIGHT
-    elif mote4_pos == 2:
-        channels.get("4").update({"direction": Position.top, "start": 16, "stop": 32})
-    # SIDE LEFT TOP
-    elif mote4_pos == 3:
-        channels.get("4").update({"direction": Position.left, "start": 0, "stop": 16})
-    # SIDE LEFT MIDDLE
-    elif mote4_pos == 4:
-        channels.get("4").update({"direction": Position.left, "start": 8, "stop": 24})
-    # SIDE LEFT BOTTOM
-    elif mote4_pos == 5:
-        channels.get("4").update({"direction": Position.left, "start": 16, "stop": 32})
-    # SIDE RIGHT TOP
-    elif mote4_pos == 6:
-        channels.get("4").update({"direction": Position.right, "start": 0, "stop": 16})
-    # SIDE RIGHT MIDDLE
-    elif mote4_pos == 7:
-        channels.get("4").update({"direction": Position.right, "start": 8, "stop": 24})
-    # SIDE RIGHT BOTTOM
-    elif mote4_pos == 8:
-        channels.get("4").update({"direction": Position.right, "start": 16, "stop": 32})
-    # BOTTOM LEFT
-    elif mote4_pos == 9:
-        channels.get("4").update({"direction": Position.bottom, "start": 0, "stop": 16})
-    # BOTTOM MIDDLE
-    elif mote4_pos == 10:
-        channels.get("4").update({"direction": Position.bottom, "start": 8, "stop": 24})
-    # BOTTOM RIGHT
-    elif mote4_pos == 11:
-        channels.get("4").update({"direction": Position.bottom, "start": 16, "stop": 32})
+                        invert = v["invert"]
+                        inv_index = 0
+                        if invert == "true":
+                            inv_index = 15
+
+                        direction = v["direction"]
+                        start = v["start"]
+                        stop = v["stop"]
+
+                        index = 0
+                        while start < stop:
+                            if direction == Direction.TOP:
+                                pixel = image.getpixel((start, 0))
+                            elif direction == Direction.LEFT:
+                                pixel = image.getpixel((0, start))
+                            elif direction == Direction.RIGHT:
+                                pixel = image.getpixel((WIDTH - 1, start))
+                            elif direction == Direction.BOTTOM:
+                                pixel = image.getpixel((start, HEIGHT - 1))
+
+                            mote.set_pixel(channel, abs(index - inv_index), pixel[0], pixel[1], pixel[2], self.settings.get_brightness())
+                            start += 1
+                            index += 1
+                    mote.show()
 
 
 if __name__ == '__main__':
-    monitor = xbmc.Monitor()
-
-    if mode == 0:
-        if color == 0:
-            clear_mote()
-        for c in channels.items():
-            for pixel in range(NUM_PIXEL):
-                set_static_color(channels, pixel, colors[color][0], colors[color][1], colors[color][2], brightness)
-        mote.show()
-
-    elif mode == 1:
-        while not monitor.abortRequested():
-            if monitor.waitForAbort(0.6):
-                break
-
-            capture = xbmc.RenderCapture()
-            capture.capture(width, height)
-            pixels = capture.getImage(1000)
-
-            if not pixels:
-                if color == 0:
-                    clear_mote()
-                else:
-                    for pixel in range(NUM_PIXEL):
-                        set_static_color(channels, pixel, colors[color][0], colors[color][1], colors[color][2], brightness)
-                    mote.show()
-            else:
-                image = Image.frombytes("RGBA", (width, height), str(pixels), "raw", "BGRA")
-
-                for k, v in channels.items():
-                    channel = v["channel"]
-
-                    invert = v["invert"]
-                    inv_index = 0
-                    if invert == "true":
-                        inv_index = 15
-
-                    direction = v["direction"]
-                    start = v["start"]
-                    stop = v["stop"]
-
-                    index = 0
-                    while start < stop:
-                        if direction == Position.top:
-                            pixel = image.getpixel((start, 0))
-                        elif direction == Position.left:
-                            pixel = image.getpixel((0, start))
-                        elif direction == Position.right:
-                            pixel = image.getpixel((width - 1, start))
-                        elif direction == Position.bottom:
-                            pixel = image.getpixel((start, height-1))
-
-                        mote.set_pixel(channel, abs(index-inv_index), pixel[0], pixel[1], pixel[2], brightness)
-                        start += 1
-                        index += 1
-                        
-                    mote.show()
-        clear_mote()
+    server = BiasLightingMonitor()
+    server.main()
---
+++ settings.py
@@ -0,0 +1,151 @@
+from enum import Enum
+import xbmc
+import xbmcaddon
+
+
+class Direction(Enum):
+    TOP = 1
+    LEFT = 2
+    RIGHT = 3
+    BOTTOM = 4
+
+class Position(Enum):
+    TOP_LEFT = (0, Direction.TOP, 0, 16)
+    TOP_MIDDLE = (1, Direction.TOP, 8, 24)
+    TOP_RIGHT = (2, Direction.TOP, 16, 32)
+    SIDE_RIGHT_TOP = (3, Direction.RIGHT, 0, 16)
+    SIDE_RIGHT_MIDDLE = (4, Direction.RIGHT, 8, 24)
+    SIDE_RIGHT_BOTTOM = (5, Direction.RIGHT, 16, 32)
+    SIDE_LEFT_TOP = (6, Direction.LEFT, 0, 16)
+    SIDE_LEFT_MIDDLE = (7, Direction.LEFT, 8, 24)
+    SIDE_LEFT_BOTTOM = (8, Direction.LEFT, 16, 32)
+    BOTTOM_LEFT = (9, Direction.BOTTOM, 0, 16)
+    BOTTOM_MIDDLE = (10, Direction.BOTTOM, 8, 24)
+    BOTTOM_RIGHT = (11, Direction.BOTTOM, 16, 32)
+
+    def __init__(self, position, direction, start, end):
+        self.direction = direction
+        self.position = position
+        self.start = start
+        self.end = end
+
+
+ADDON_ID = 'script.service.biaslightmote'
+
+settings = xbmcaddon.Addon(id=ADDON_ID)
+
+
+class Color:
+    red = None
+    green = None
+    blue = None
+
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+
+class Settings:
+    def __init__(self):
+        self.colors = []
+        self.colors.append(Color(0, 0, 0))
+        self.colors.append(Color(255, 0, 0))
+        self.colors.append(Color(0, 255, 0))
+        self.colors.append(Color(0, 0, 255))
+        self.colors.append(Color(255, 255, 0))
+        self.colors.append(Color(255, 255, 255))
+
+        self._brightness = None
+        self._color = None
+        self._mode = None
+        self._mote4_pos = None
+        self._mote4_inv = None
+        self._mote4 = None
+        self._mote3_inv = None
+        self._mote3_pos = None
+        self._mote3 = None
+        self._mote2_pos = None
+        self._mote2 = None
+        self._mote2_inv = None
+        self._mote1_pos = None
+        self._mote1_inv = None
+        self._mote1 = None
+        self._channels = {}
+
+        self.readSettings()
+
+    def get_mode(self):
+        return self._mode
+
+    def get_channels(self):
+        return self._channels
+
+    def get_color(self):
+        return self._color
+
+    def get_brightness(self):
+        return self._brightness
+
+    def readSettings(self):
+        self._mote1 = settings.getSetting("mote1")
+        self._mote1_pos = int(settings.getSetting("mote1_position"))
+        self._mote1_inv = settings.getSetting("mote1_invert")
+
+        self._mote2 = settings.getSetting("mote2")
+        self._mote2_pos = int(settings.getSetting("mote2_position"))
+        self._mote2_inv = settings.getSetting("mote2_invert")
+
+        self._mote3 = settings.getSetting("mote3")
+        self._mote3_pos = int(settings.getSetting("mote3_position"))
+        self._mote3_inv = settings.getSetting("mote3_invert")
+
+        self._mote4 = settings.getSetting("mote4")
+        self._mote4_pos = int(settings.getSetting("mote4_position"))
+        self._mote4_inv = settings.getSetting("mote4_invert")
+
+        self._mode = int(settings.getSetting("mode"))
+        _color = int(settings.getSetting("color"))
+
+        if _color < 6:
+            self._color = self.colors[_color]
+        else:
+            custom_color_red = int(settings.getSetting("color_custom_red"))
+            custom_color_green = int(settings.getSetting("color_custom_green"))
+            custom_color_blue = int(settings.getSetting("color_custom_blue"))
+
+            self._color = Color(custom_color_red, custom_color_green, custom_color_blue)
+
+        self._brightness = float(float(settings.getSetting("brightness")) / float(100))
+
+        self.set_motes_channels()
+
+    def set_motes_channels(self):
+        if self._mote1 == "true":
+            self._channels.update({1: {"invert": self._mote1_inv}})
+            channel1 = self._channels.get(1)
+            self.set_mote_position(channel1, self._mote1_pos)
+
+        if self._mote2 == "true":
+            self._channels.update({2: {"invert": self._mote2_inv}})
+            channel2 = self._channels.get(2)
+            self.set_mote_position(channel2, self._mote2_pos)
+
+        if self._mote3 == "true":
+            self._channels.update({3: {"invert": self._mote3_inv}})
+            channel3 = self._channels.get(3)
+            self.set_mote_position(channel3, self._mote3_pos)
+
+        if self._mote4 == "true":
+            self._channels.update({4: {"invert": self._mote4_inv}})
+            channel4 = self._channels.get(4)
+            self.set_mote_position(channel4, self._mote4_pos)
+
+    def set_mote_position(self, channel, mote_pos):
+        pos = None
+
+        for name, member in Position.__members__.items():
+            if member.position == mote_pos:
+                pos = member
+
+        channel.update({"direction": pos.direction, "start": pos.start, "stop": pos.end})
